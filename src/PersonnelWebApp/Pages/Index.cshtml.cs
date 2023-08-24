b836479169381f5b3ed82116600b8da3fcf41913
QuickOrBeDead <boraakgn@gmail.com>
2023-08-24 15:54:32 +0300

    add login page

--- src/PersonnelWebApp/Pages/Index.cshtml.cs
+++ src/PersonnelWebApp/Pages/Index.cshtml.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace PersonnelWebApp.Pages;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
-namespace PersonnelWebApp.Pages
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+[AllowAnonymous]
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    [BindProperty]
+    public string UserName { get; set; }
+
+    [BindProperty, DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public string Message { get; set; }
+
+    public async Task<IActionResult> OnPost()
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        if (!string.IsNullOrWhiteSpace(UserName))
         {
-            _logger = logger;
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new(ClaimTypes.Name, UserName) }, CookieAuthenticationDefaults.AuthenticationScheme)));
+            return RedirectToPage("/privacy");
         }
 
-        public void OnGet()
-        {
-
-        }
+        Message = "Invalid User name or Password";
+        return Page();
     }
 }
