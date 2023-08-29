699f6fe39b6e100d2bebe3d3cc8b8e771284fab9
QuickOrBeDead <boraakgn@gmail.com>
2023-08-29 11:13:13 +0300

    add mongodb client and collection

---
+++ src/PersonnelWebApp/Infrastructure/Model/Personnel.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonnelWebApp.Infrastructure.Model;
+
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+public sealed class Personnel
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Password { get; set; }
+}
--- src/PersonnelWebApp/Program.cs
+++ src/PersonnelWebApp/Program.cs
@@ -1,9 +1,21 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 
+using MongoDB.Driver;
+
+using PersonnelWebApp.Infrastructure.Model;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+var mongoUrl = MongoUrl.Create(builder.Configuration.GetConnectionString("PersonnelDb"));
+
+builder.Services.AddSingleton<IMongoClient>(_ => new MongoClient(mongoUrl));
+builder.Services.AddSingleton(x => x.GetRequiredService<IMongoClient>().GetDatabase(mongoUrl.DatabaseName));
+builder.Services.AddSingleton(x => x.GetRequiredService<IMongoDatabase>().GetCollection<Personnel>(nameof(Personnel)));
+builder.Services.AddSingleton<IPasswordHasher<string>>(_ => new PasswordHasher<string>());
 
 builder.Services.Configure<CookiePolicyOptions>(
     options =>
