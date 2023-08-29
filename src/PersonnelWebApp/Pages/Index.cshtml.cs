6c79e6d2d6d2f0485dffaf2ae271d29b36ae9e57
QuickOrBeDead <boraakgn@gmail.com>
2023-08-29 11:15:14 +0300

    add username and password validation

--- src/PersonnelWebApp/Pages/Index.cshtml.cs
+++ src/PersonnelWebApp/Pages/Index.cshtml.cs
@@ -8,10 +8,19 @@ using System.Security.Claims;
 
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+
+using MongoDB.Driver;
+
+using PersonnelWebApp.Infrastructure.Model;
 
 [AllowAnonymous]
 public class IndexModel : PageModel
 {
+    private readonly IMongoCollection<Personnel> _mongoPersonnelCollection;
+
+    private readonly IPasswordHasher<string> _passwordHasher;
+
     [BindProperty]
     public string UserName { get; set; }
 
@@ -20,15 +29,28 @@ public class IndexModel : PageModel
 
     public string Message { get; set; }
 
+    public IndexModel(IMongoCollection<Personnel> mongoPersonnelCollection, IPasswordHasher<string> passwordHasher)
+    {
+        _mongoPersonnelCollection = mongoPersonnelCollection ?? throw new ArgumentNullException(nameof(mongoPersonnelCollection));
+        _passwordHasher = passwordHasher ?? throw new ArgumentNullException(nameof(passwordHasher));
+    }
+
     public async Task<IActionResult> OnPost()
     {
-        if (!string.IsNullOrWhiteSpace(UserName))
+        var personnel =_mongoPersonnelCollection.AsQueryable().Where(x => x.UserName == UserName).Select(x => new { x.UserName, x.Password }).SingleOrDefault();
+        if (personnel == null)
         {
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new(ClaimTypes.Name, UserName) }, CookieAuthenticationDefaults.AuthenticationScheme)));
-            return RedirectToPage("/privacy");
+            Message = "User not found";
+            return Page();
         }
 
-        Message = "Invalid User name or Password";
-        return Page();
+        if (_passwordHasher.VerifyHashedPassword(UserName, personnel.Password, Password) == PasswordVerificationResult.Failed)
+        {
+            Message = "Invalid password";
+            return Page();
+        }
+
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new(ClaimTypes.Name, UserName) }, CookieAuthenticationDefaults.AuthenticationScheme)));
+        return RedirectToPage("/privacy");
     }
 }
