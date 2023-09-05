89eac34eadd96d931a9cbd645521f1d243332137
QuickOrBeDead <boraakgn@gmail.com>
2023-09-05 15:05:37 +0300

    move db initialization to DbInitializationService

---
+++ src/PersonnelWebApp/Infrastructure/Service/DbInitializationService.cs
@@ -0,0 +1,37 @@
+﻿namespace PersonnelWebApp.Infrastructure.Service;
+
+using PersonnelWebApp.Infrastructure.Model;
+
+using MongoDB.Driver;
+
+using Microsoft.AspNetCore.Identity;
+
+public interface IDbInitializationService
+{
+    void InitDb();
+}
+
+public sealed class DbInitializationService : IDbInitializationService
+{
+    private readonly IMongoCollection<Personnel> _personnelCollection;
+
+    private readonly IPasswordHasher<string> _passwordHasher;
+
+    public DbInitializationService(IMongoCollection<Personnel> personnelCollection, IPasswordHasher<string> passwordHasher)
+    {
+        _personnelCollection = personnelCollection ?? throw new ArgumentNullException(nameof(personnelCollection));
+        _passwordHasher = passwordHasher ?? throw new ArgumentNullException(nameof(passwordHasher));
+    }
+
+    public void InitDb()
+    {
+        if (_personnelCollection.CountDocuments(FilterDefinition<Personnel>.Empty) == 0)
+        {
+            _personnelCollection.InsertOne(new Personnel
+                                              {
+                                                  UserName = "admin",
+                                                  Password = _passwordHasher.HashPassword("admin", "admin")
+                                              });
+        }
+    }
+}
--- src/PersonnelWebApp/Program.cs
+++ src/PersonnelWebApp/Program.cs
@@ -1,9 +1,10 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 
 using MongoDB.Driver;
 
 using PersonnelWebApp.Infrastructure.Model;
-using Microsoft.AspNetCore.Identity;
+using PersonnelWebApp.Infrastructure.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,7 @@ builder.Services.AddSingleton<IMongoClient>(_ => new MongoClient(mongoUrl));
 builder.Services.AddSingleton(x => x.GetRequiredService<IMongoClient>().GetDatabase(mongoUrl.DatabaseName));
 builder.Services.AddSingleton(x => x.GetRequiredService<IMongoDatabase>().GetCollection<Personnel>(nameof(Personnel)));
 builder.Services.AddSingleton<IPasswordHasher<string>>(_ => new PasswordHasher<string>());
+builder.Services.AddSingleton<IDbInitializationService, DbInitializationService>();
 
 builder.Services.Configure<CookiePolicyOptions>(
     options =>
@@ -35,23 +37,12 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
 
-var personnelCollection = app.Services.GetRequiredService<IMongoCollection<Personnel>>();
-if (personnelCollection.CountDocuments(FilterDefinition<Personnel>.Empty) == 0)
-{
-    var passwordHasher = app.Services.GetRequiredService<IPasswordHasher<string>>();
-    personnelCollection.InsertOne(new Personnel
-                                      {
-                                          UserName = "admin",
-                                          Password = passwordHasher.HashPassword("admin", "admin")
-                                      });
-}
+app.Services.GetRequiredService<IDbInitializationService>().InitDb();
 
 app.Run();
