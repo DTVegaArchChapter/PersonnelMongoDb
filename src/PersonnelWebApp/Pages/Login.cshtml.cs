719678d746be08a0982167eaf3d799415daf125b
QuickOrBeDead <boraakgn@gmail.com>
2023-09-05 21:29:21 +0300

    add logout

--- src/PersonnelWebApp/Pages/Login.cshtml.cs
+++ src/PersonnelWebApp/Pages/Login.cshtml.cs
@@ -1,41 +1,56 @@
+namespace PersonnelWebApp.Pages;
+
+using PersonnelWebApp.Infrastructure.Service;
+
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using PersonnelWebApp.Infrastructure.Service;
+
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 
-namespace PersonnelWebApp.Pages
+public class LoginModel : PageModel
 {
-    public class LoginModel : PageModel
+    private readonly IUserService _userService;
+
+    [BindProperty]
+    public string UserName { get; set; }
+
+    [BindProperty, DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public string Message { get; set; }
+
+    public LoginModel(IUserService userService)
     {
-        private readonly IUserService _userService;
+        _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+    }
 
-        [BindProperty]
-        public string UserName { get; set; }
+    public async Task<IActionResult> OnGetLogoutAsync()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
-        [BindProperty, DataType(DataType.Password)]
-        public string Password { get; set; }
+        return Redirect("/");
+    }
 
-        public string Message { get; set; }
-
-        public LoginModel(IUserService userService)
+    public async Task<IActionResult> OnPostAsync(string returnUrl)
+    {
+        var (message, success) = _userService.LoginUser(UserName, Password);
+        if (!success)
         {
-            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+            Message = message;
+            return Page();
         }
 
-        public async Task<IActionResult> OnPost()
-        {
-            var (message, success) = _userService.LoginUser(UserName, Password);
-            if (!success)
-            {
-                Message = message;
-                return Page();
-            }
-
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(new ClaimsIdentity(new List<Claim> { new(ClaimTypes.Name, UserName) }, CookieAuthenticationDefaults.AuthenticationScheme)));
-            return RedirectToPage("/privacy");
-        }
+        var principal = new ClaimsPrincipal(
+            new ClaimsIdentity(
+                new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, UserName)
+                    }, 
+                CookieAuthenticationDefaults.AuthenticationScheme));
+        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+        return Redirect(returnUrl ?? "/");
     }
 }
