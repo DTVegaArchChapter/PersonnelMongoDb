82a50fe8089710b782ddd5ac6e218f14f20a1437
Tuncay Cem Uzun <tuncaycemuzun@outlook.com>
2023-09-19 15:10:13 +0300

    Vacation add/delete/list and login validation

---
+++ src/PersonnelWebApp/Infrastructure/Model/Vacation.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace PersonnelWebApp.Infrastructure.Model
+{
+    public class Vacation
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime StartDate { get; set; }
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime EndDate { get; set; }
+    }
+}
--- src/PersonnelWebApp/Infrastructure/Service/UserService.cs
+++ src/PersonnelWebApp/Infrastructure/Service/UserService.cs
@@ -5,6 +5,7 @@ using PersonnelWebApp.Infrastructure.Model;
 using MongoDB.Driver;
 
 using Microsoft.AspNetCore.Identity;
+using MongoDB.Bson;
 
 public interface IUserService
 {
@@ -13,6 +14,9 @@ public interface IUserService
     int GetCount();
     Personnel GetPersonnel(string id);
     Task AddOrUpdatePersonnel(Personnel personnel);
+    Task AddPersonelVacation(Vacation vacation, string userName);
+    (IEnumerable<Vacation> Data, int Count) GetUserVacations(int pageNumber, int pageSize, string userName);
+    Task DeletePersonelVacation(string userName, string vacationId);
 }
 
 public sealed class UserService : IUserService
@@ -29,7 +33,7 @@ public sealed class UserService : IUserService
 
     public (string, bool) LoginUser(string userName, string password)
     {
-        var personnel = _mongoPersonnelCollection.AsQueryable().Where(x => x.UserName == userName).Select(x => new { x.UserName, x.Password }).SingleOrDefault();
+        var personnel = _mongoPersonnelCollection.AsQueryable().Where(x => x.UserName == userName).Select(x => new { x.UserName, x.Password, x.Vacations }).SingleOrDefault();
         if (personnel == null)
         {
             return ("User not found", false);
@@ -38,6 +42,16 @@ public sealed class UserService : IUserService
         if (_passwordHasher.VerifyHashedPassword(userName, personnel.Password, password) == PasswordVerificationResult.Failed)
         {
             return ("Invalid password", false);
+        }
+
+        DateTime now = DateTime.UtcNow;
+
+        foreach (var vacation in personnel.Vacations)
+        {
+            if (now >= vacation.StartDate && now <= vacation.EndDate)
+            {
+                return ("You are currently on vacation. Access denied.", false);
+            }
         }
 
         return (string.Empty, true);
@@ -76,5 +90,43 @@ public sealed class UserService : IUserService
                 await _mongoPersonnelCollection.ReplaceOneAsync(x => x.Id == personnel.Id, personnel);
             }
         }
+    }
+
+    public (IEnumerable<Vacation> Data, int Count) GetUserVacations(int pageNumber, int pageSize, string userName)
+    {
+        var query = _mongoPersonnelCollection.AsQueryable().Where(x => x.UserName == userName);
+
+        var test = _mongoPersonnelCollection.AsQueryable().ToList();
+
+        var vacations = query.SelectMany(x => x.Vacations)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        var count = query.SelectMany(x => x.Vacations)
+            .Count();
+
+        return (vacations, count);
+    }
+
+    public async Task AddPersonelVacation(Vacation vacation, string userName)
+    {
+        vacation.Id = ObjectId.GenerateNewId().ToString();
+        var filter = Builders<Personnel>.Filter.Eq(x => x.UserName, userName);
+        var update = Builders<Personnel>.Update.Push(x => x.Vacations, vacation);
+
+        await _mongoPersonnelCollection.UpdateOneAsync(filter, update);
+    }
+
+    public async Task DeletePersonelVacation(string userName, string vacationId)
+    {
+        var filter = Builders<Personnel>.Filter.And(
+        Builders<Personnel>.Filter.Eq(x => x.UserName, userName),
+        Builders<Personnel>.Filter.ElemMatch(x => x.Vacations, Builders<Vacation>.Filter.Eq(x => x.Id, vacationId))
+    );
+
+        var update = Builders<Personnel>.Update.PullFilter(x => x.Vacations, Builders<Vacation>.Filter.Eq(x => x.Id, vacationId));
+
+        var result = await _mongoPersonnelCollection.UpdateOneAsync(filter, update);
     }
 }
